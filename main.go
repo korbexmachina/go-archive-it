7333a91d955e735412a38324e6d31439e1b8b009
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-07-30 21:06:08 -0700

    fixed file permissions for config generation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,7 +11,7 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-type Template struct {
+type Config struct {
 	vault_path     []string
 	archive_path   string
 	archive_type   int
@@ -20,30 +21,37 @@ type Template struct {
 func main() {
 	start := time.Now()
 	// config_path, err := filepath.Abs("~/.config/go-archive-it/config.yaml")
-	config_path, err := filepath.Abs("./go-archive-it/config.yaml") // test path
+	config_path, _ := filepath.Abs("./test-conf/config.yaml") // test path
 
-	if os.IsNotExist(err) {
+	if _, err := os.Stat(config_path); os.IsNotExist(err) {
 		log.Print("creating config directory")
 
-		os.Mkdir(filepath.Dir(config_path), os.ModePerm)
+		err := os.MkdirAll(filepath.Dir(config_path), os.ModePerm)
 		if err != nil {
-			log.Fatal("Unable to create directory: %v", err)
+			log.Fatalf("Unable to create directory: %v", err)
 		}
 
-		template := Template{
+		config := Config{
 			vault_path: []string{"~/notes, ~/dev"},
-			archive_path: "~/go-archive-it",
+			archive_path: "./archive",
 			archive_type: 2,
 		}
 
-		err := os.WriteFile("filename.txt", template, 0755)
+		data, err2 := yaml.Marshal(&config)
+
+		if err2 != nil {
+			log.Fatalf("Failed to serialize data: %v", err2)
+		}
+
+		err = ioutil.WriteFile(config_path, data, os.ModeAppend | 0664)
 		if err != nil {
-			log.Fatal("Unable to write file: %v", err)
+			log.Fatalf("Unable to write file: %v", err)
 		}
 
 		fmt.Printf("Config Created at %s, make any neccesary changes and run the program again", config_path)
 		os.Exit(0)
 	}
+
 	elapsed := time.Since(start)
 	log.Printf("Archive(s) created in [[ %s ]]", elapsed)
 }
