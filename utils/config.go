ae64e4db77c6642d2339ce2339fc0e73ec80cfce
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-07-31 01:05:27 -0700

    added utils

---
+++ utils/config.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	VaultPath []string
+	ArchivePath string
+	ArchiveType uint8
+}
+
+
+func ConfigExists(configPath string) {
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Print("creating config directory")
+
+		err := os.MkdirAll(filepath.Dir(configPath), os.ModePerm)
+		if err != nil {
+			log.Fatalf("Unable to create directory: %v", err)
+		}
+
+		config := Config{
+			VaultPath: []string{"~/notes", "~/dev"},
+			ArchivePath: "./archive",
+			ArchiveType: 2,
+		}
+
+		c, err := yaml.Marshal(config)
+
+		if err != nil {
+			log.Fatalf("Failed to serialize data: %v", err)
+		}
+
+		err = ioutil.WriteFile(configPath, c, os.ModeAppend | 0664)
+		if err != nil {
+			log.Fatalf("Unable to write file: %v", err)
+		}
+
+		log.Printf("Config Created at %s, make any neccesary changes and run the program again", configPath)
+		os.Exit(0)
+	}
+}
